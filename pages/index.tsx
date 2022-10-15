e71ed2e7b89f4acc19f7cef997ad433129e42f00
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2022-10-15 18:12:30 +0800

    added image

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,52 +13,12 @@ const Home: NextPage = () => {
           </Head>
 
           <main className={styles.main}>
-              <h1 className={styles.title}>
-                  Welcome to <a href="https://nextjs.org">Next.js!</a>
-              </h1>
-
-              <p className={styles.description}>
-                  Get started by editing <code className={styles.code}>pages/index.tsx</code>
-              </p>
-
-              <div className={styles.grid}>
-                  <a href="https://nextjs.org/docs" className={styles.card}>
-                      <h2>Documentation &rarr;</h2>
-                      <p>Find in-depth information about Next.js features and API.</p>
-                  </a>
-
-                  <a href="https://nextjs.org/learn" className={styles.card}>
-                      <h2>Learn &rarr;</h2>
-                      <p>Learn about Next.js in an interactive course with quizzes!</p>
-                  </a>
-
-                  <a
-                      href="https://github.com/vercel/next.js/tree/canary/examples"
-                      className={styles.card}>
-                      <h2>Examples &rarr;</h2>
-                      <p>Discover and deploy boilerplate example Next.js projects.</p>
-                  </a>
-
-                  <a
-                      href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                      className={styles.card}>
-                      <h2>Deploy &rarr;</h2>
-                      <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-                  </a>
-              </div>
+              <h1 className={styles.title}>Welcome to Myselfs</h1>
+              <img src={`${process.env.NEXT_PUBLIC_FAVICON}/favicon.ico`} alt="" />
+              <img src="https://scontent.fceb2-1.fna.fbcdn.net/v/t39.30808-6/295174927_2204961829664450_3190420111181056787_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGryMmFP6ARkSwBswf9aCmjMTLx6kaa7VQxMvHqRprtVGUBxJW9R3XrF6jeCGm_Boww3BUtSWOhLyHIZYEwoxej&_nc_ohc=0D0ilCe_hx0AX-VIwfR&_nc_ht=scontent.fceb2-1.fna&oh=00_AT-LIopPJkPjt4daYJ9EGjTtHryn3DoTiYsSCqo9QyPeKA&oe=634D761B"
+                  alt=""
+              />
           </main>
-
-          <footer className={styles.footer}>
-              <a
-                  href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                  target="_blank"
-                  rel="noopener noreferrer">
-                  Powered by{' '}
-                  <span className={styles.logo}>
-                      <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-                  </span>
-              </a>
-          </footer>
       </div>
   )
 }
